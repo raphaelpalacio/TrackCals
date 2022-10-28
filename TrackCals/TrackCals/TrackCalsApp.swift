480fc1bd86faa09be73ad77b1d260d8b7e373419
Raphael Palacio <54969177+raphaelpalacio@users.noreply.github.com>
2022-10-27 23:57:18 -0400

    Added new Xcode File

---
+++ TrackCals/TrackCals/TrackCalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackCalsApp.swift
+//  TrackCals
+//
+//  Created by Raphael Palacio on 10/27/22.
+//
+
+import SwiftUI
+
+@main
+struct TrackCalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
